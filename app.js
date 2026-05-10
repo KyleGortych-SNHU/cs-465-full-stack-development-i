2bb91fdf9fc6cae5694cd0f5302a7e12bb51543f
kylegortych <kyle.gortych@gmail.com>
2026-05-10 17:03:52 -0400

    adding CI workflow for server health and static asset checks

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// for git action CI testing
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

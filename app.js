034762df57afcd63f1a66ae42920d57e20d21e47
kylegortych <kyle.gortych@gmail.com>
2026-05-17 22:57:05 -0400

    added new screenshot for module2 working

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
-let handlebars = require('bhs');
+let handlebars = require('hbs');
 
-let indexRouter = require('/app_server/routes/index');
-let usersRouter = require('/app_server/routes/users');
+let indexRouter = require('./app_server/routes/index');
+let usersRouter = require('./app_server/routes/users');
 let travelRouter = require('./app_server/routes/travel');
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server/views'));
 app.set('view engine', 'hbs');
 
 // register handlebars partials
-handlebars.registerPartials(__dirname + '/app_server/view/partials');
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.use(logger('dev'));
 app.use(express.json());

3193d1cad5736fe46d9fae41b3a0ecea49b615a9
kylegortych <kyle.gortych@gmail.com>
2026-05-21 14:52:50 -0400

    Refactored the travel.js controller file to use fs.readFileSync method to parse and read the trips.json file.

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,9 @@
+let fs = require('fs');
+let trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-  res.render('travel', {title: 'travlr Getaways'});
+  res.render('travel', {title: 'Travlr Getaways', trips});
 };
 
 module.exports = {

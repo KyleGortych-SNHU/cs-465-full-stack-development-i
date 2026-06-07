e0a45d17e7327815cb77bfa57b735b8ae1b22c29
kylegortych <kyle.gortych@gmail.com>
2026-06-07 16:21:39 -0400

    updated travel.js controller to use the new api endpoints rather than static json file

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,11 +1,27 @@
-let fs = require('fs');
-let trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+//let fs = require('fs');
+//let trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+const { json } = require("express");
+
+const tripsEndpoint = "http://localhost:3000/api/trips";
+const options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
+};
 
 /* GET travel view */
-const travel = (req, res) => {
-  res.render('travel', {title: 'Travlr Getaways', trips});
+const travel = async function (req, res, next) {
+  // console.log("TRAVEL CONTROLLER BEGIN");
+  await fetch(tripsEndpoint, options)
+      .then((res) => res.json())
+      .then((json) => {
+        res.render('travel', {title: 'Travlr Getaways', trips});
+      })
+      .catch((err) => res.status(500).send(err.message));
 };
 
 module.exports = {
-  travel
+  travel,
 };

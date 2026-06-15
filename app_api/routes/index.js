b7c8a837bbf2c7335332bd6d799b8cb93e40067a
kylegortych <kyle.gortych@gmail.com>
2026-06-15 17:00:53 -0400

    Finished guide for module 6 need to resolve issues.

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -54,6 +54,7 @@ router
  */
 router
     .route("/trips/:tripCode")
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;

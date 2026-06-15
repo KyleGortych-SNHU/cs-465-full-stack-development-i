683d9d794f1b44a803075c9bb117e9e8bcc06ca2
kylegortych <kyle.gortych@gmail.com>
2026-06-15 15:15:01 -0400

    implmented files up to pg 165 in guide

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -36,7 +36,8 @@ const tripsController = require("../controllers/trips");
  */
 router
     .route("/trips")
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
 
 /**
  * Route: GET /trips/:tripCode

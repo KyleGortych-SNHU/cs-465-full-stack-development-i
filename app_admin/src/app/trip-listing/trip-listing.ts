1d1a32a44e8cf8ff4be7eb5fea6f7e57106419b5
kylegortych <kyle.gortych@gmail.com>
2026-06-14 21:36:53 -0400

    implmented files up to pg 149 in guide

--- app_admin/src/app/trip-listing/trip-listing.ts
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -1,21 +1,49 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { trips } from '../data/trips';
+import { CommonModule } from '@angular/common';
 import { TripCard } from '../trip-card/trip-card';
+
+import { TripDataService } from '../services/trip-data';
+import { Trip } from '../models/trip';
 
 @Component({
   selector: 'app-trip-listing',
+  standalone: true,
   imports: [CommonModule, TripCard],
   templateUrl: './trip-listing.html',
   styleUrl: './trip-listing.css',
+  providers: [TripDataService]
 })
 
 export class TripListing implements OnInit {
-  trips: Array<any> = trips;
+  trips!: Trip[];
+  message: string = '';
 
-  constructor() {}
+  constructor(private tripDataService: TripDataService) {
+    console.log('trip-listing constructor');
+  }
+
+  private getStuff(): void {
+    this.tripDataService.getTrips()
+      .subscribe({
+        next: (value: any) => {
+          this.trips = value;
+          if(value.length > 0){
+            this.message = 'There are ' + value.length + ' trips available';
+          } else {
+            this.message = 'There were no trips retireved from the database';
+          }
+          console.log(this.message);
+        },
+        error: (error: any) => {
+          console.log('Error: ' + error);
+        }
+      })
+  }
 
   ngOnInit(): void {
-
+    console.log('ngOnInit');
+    this.getStuff();
   }
 }
+
+
